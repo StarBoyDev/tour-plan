1d1ce1bb0cab920a1814d6f9ba31d0123e18e92e
Taras <taraskashanyk@ukr.net>
2020-10-05 21:01:15 +0300

    create: form validation

--- js/main.js
+++ js/main.js
@@ -55,4 +55,24 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  // Form validate
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите имя",
+          minlength: "Имя должно быть не короче 2-вух букв",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+      },
+    });
+  });
 });
